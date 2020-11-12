49a09f6dc2ee5ecbb065b2ac34ad224cf8125cca
Katarzyna Michalowska <kami@powel.com>
2020-11-12 19:49:50 +0100

    updated class Bank

--- BankTransfers/Bank.cs
+++ BankTransfers/Bank.cs
@@ -5,23 +5,31 @@ namespace BankTransfers
     public class Bank
     {
         private readonly List<BankAccount> _accounts;
+        private readonly List<Transfer> _transfers;
+
 
         public Bank()
         {
             _accounts = new List<BankAccount>();
+            _transfers = new List<Transfer>();
         }
 
         public BankAccount CreateAccount(string accountName)
         {
             BankAccount newAccount = new BankAccount(accountName);
             _accounts.Add(newAccount);
-            
+
             return newAccount;
         }
 
         public List<BankAccount> GetAccount()
         {
             return _accounts;
+        }
+
+        public void RegisterTransfer(Transfer transfer)
+        {
+            _transfers.Add(transfer);
         }
 
         public BankAccount GetBankAccount(int index)
@@ -32,6 +40,10 @@ namespace BankTransfers
             }
 
             return null;
+        }
+        public List<Transfer> GetTransfers()
+        {
+            return _transfers;
         }
     }
 }
